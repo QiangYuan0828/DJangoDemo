cc7b0e194c9b63fa4a6f71583831828d4abb74de
QiangYuan0828 <40728235+QiangYuan0828@users.noreply.github.com>
2023-02-11 23:54:09 -0500

    Delete polls directory

--- polls/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. Youre at the polls index.")
-    # response a message to user in webs
